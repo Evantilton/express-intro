06fdfbe37b73c4c330ea8b47899e6ee231fc329b
Evan Tilton <evantilton@gmail.com>
2019-10-08 15:54:36 -0500

    more information

--- server/server.js
+++ server/server.js
@@ -3,10 +3,12 @@ let express = require('express')
 
 const app = express();
 
+app.use(express.static('server/public'));
 app.get('/', (req, res) => {
     console.log('I have been hit!')
     res.send('This is the base route!');
 });
+
 app.get('/welcome', (req, res) => {
     console.log('I have been hit!')
     res.sendStatus(200);
