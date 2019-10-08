97fbf9a296a0cfd50e8cf86ecf40865ba38b7d4c
Evan Tilton <evantilton@gmail.com>
2019-10-08 16:13:15 -0500

    cool, linked it

--- server/server.js
+++ server/server.js
@@ -5,7 +5,14 @@ const app = express();
 
 const PORT = 5000;
 
-app.use(express.static('server/public'));
+let quotesData = [
+    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+
+
 app.get('/', (req, res) => {
     console.log('I have been hit!')
     res.send('This is the base route!');
@@ -18,10 +25,16 @@ app.get('/welcome', (req, res) => {
     
 });
 
+app.get('/first', (req, res) => {
+    console.log('I have been hit!')
+    res.send(quotesData[1].quote);
+    
+});
 
-
+app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
     console.log('Up and running on port', PORT);
 });
+
 
